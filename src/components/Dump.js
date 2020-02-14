fbd4d8f0d712a0b9e3bd80a15bafa1410290fc65
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2020-02-14 12:37:23 -0500

    updated dump emoji so errors stopped

--- src/components/Dump.js
+++ src/components/Dump.js
@@ -11,7 +11,12 @@ const Dump = props => (
   >
     {Object.entries(props).map(([key, val]) => (
       <pre key={key}>
-        <strong style={{ color: 'white', background: 'red' }}>{key} 💩</strong>
+        <strong style={{ color: 'white', background: 'red' }}>
+          {key}{' '}
+          <span role='img' aria-label='poop-emoji'>
+            💩
+          </span>
+        </strong>
         {JSON.stringify(val, '', ' ')}
       </pre>
     ))}
