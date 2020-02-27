15d2e24185277c6ec1586a58c0f22f282c1c0f50
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2020-02-27 17:28:44 -0500

    Added build and serve and styled components

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -64,7 +64,7 @@ export const query = graphql`
     mdx(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
-        date
+        date(formatString: "YYYY MMMM Do")
         cover {
           publicURL
         }
